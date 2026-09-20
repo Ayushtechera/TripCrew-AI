92f1d81c01abb058139bfa9e45bc5de7997fdb5c
Ayush Kashyap <ayushkashyap0109@gmail.com>
2026-09-20 18:41:33 +0530

    flight and tavily tools added

---
+++ test.py
@@ -0,0 +1,36 @@
+  
+# # res = tavily_search("Best Hotels in India")
+# # print(res)
+
+
+res = search_flights("Plan a 7 days Germany trip from India")
+
+print(res)
+
+
+
+
+# import os
+# from dotenv import load_dotenv
+# import requests
+
+# load_dotenv()
+
+# API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
+# print(f"API Key exists: {bool(API_KEY)}")
+# print(f"API Key: {API_KEY}")
+# print(f"Length: {len(API_KEY) if API_KEY else 0}")
+
+# # Direct API call
+# response = requests.get(
+#     "https://api.aviationstack.com/v1/flights",
+#     params={
+#         "access_key": API_KEY,
+#         "limit": 1,
+#         "dep_iata": "DEL",
+#         "arr_iata": "NRT"
+#     }
+# )
+
+# print("\nStatus Code:", response.status_code)
+# print("Response:", response.json())
